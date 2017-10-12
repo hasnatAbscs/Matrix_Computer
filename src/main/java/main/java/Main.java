d5e5e5da20036d0c731571a7217db717d11fb9ed
Hasnat Amir <hamir.bscs17seecs@nust.edu.pk>
2017-10-12 01:29:09 -0700

    added commit

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -2,7 +2,7 @@ package main.java;
 import java.util.*;
 
 public class Main {
-
+	//java script
     private static int rows1=0,rows2=0;
     private static int columns1=0,columns2=0;
     private static ArrayList<String> characters = new ArrayList<String>(10);
